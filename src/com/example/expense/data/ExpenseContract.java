a9708de0869a61f9e15f891915e83c86161725a0
z <z@g>
2013-09-07 14:32:03 +0800

    clean up

--- src/com/example/expense/data/ExpenseContract.java
+++ src/com/example/expense/data/ExpenseContract.java
@@ -10,21 +10,6 @@ public final class ExpenseContract {
         public static final String TABLE_NAME = "expense_category";
         public static final String COLUMN_NAME_TITLE = "title";
     }
-
-    public static abstract class PaymentMethod implements BaseColumns {
-        public static final String TABLE_NAME = "payment_method";
-        public static final String COLUMN_NAME_TITLE = "title";
-    }
-
-    public static abstract class Payment implements BaseColumns {
-        public static final String TABLE_NAME = "payment";
-        public static final String COLUMN_NAME_DATE = "date";
-        public static final String COLUMN_NAME_AMOUNT = "amount";
-        public static final String COLUMN_NAME_DESCRIPTION = "description";
-        public static final String COLUMN_NAME_PAYMENT_METHOD = "payment_method";
-        public static final String COLUMN_NAME_EXPENSE_CATEGORY = "expense_category";
-    }
-    
     
     public static abstract class Account implements BaseColumns {
         public static final String TABLE_NAME = "account";
