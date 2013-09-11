7842a1ea1933851f3096377174b045d516284d77
z <z@g>
2013-09-11 14:21:01 +0800

    implement insert and update account

--- src/com/example/expense/AccountsActivity.java
+++ src/com/example/expense/AccountsActivity.java
@@ -1,21 +1,29 @@
 package com.example.expense;
 
-import com.example.expense.content.AccountProvider;
-import com.example.expense.data.ExpenseContract;
-
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Loader;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+import com.example.expense.content.AccountProvider;
+import com.example.expense.data.ExpenseContract;
 
 //
 // http://developer.android.com/guide/topics/ui/layout/listview.html
@@ -34,15 +42,14 @@ public class AccountsActivity extends ListActivity implements LoaderCallbacks<Cu
 
     // This is the select criteria
     static final String SELECTION = null;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         // Create a progress bar to display while the list loads
         ProgressBar progressBar = new ProgressBar(this);
-        progressBar.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-                LayoutParams.WRAP_CONTENT/*, Gravity.CENTER*/));
+        progressBar.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT/*, Gravity.CENTER*/));
         progressBar.setIndeterminate(true);
         getListView().setEmptyView(progressBar);
 
@@ -52,18 +59,18 @@ public class AccountsActivity extends ListActivity implements LoaderCallbacks<Cu
 
         // For the cursor adapter, specify which columns go into which views
         String[] fromColumns = { ExpenseContract.Account.COLUMN_NAME_NAME };
-        int[] toViews = {android.R.id.text1}; // The TextView in simple_list_item_1
+        int[] toViews = { android.R.id.text1 }; // The TextView in simple_list_item_1
 
         // Create an empty adapter we will use to display the loaded data.
         // We pass null for the cursor, then update it in onLoadFinished()
-        mAdapter = new SimpleCursorAdapter(this, 
-                android.R.layout.simple_list_item_1, null,
-                fromColumns, toViews, 0);
+        mAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, null, fromColumns, toViews, 0);
         setListAdapter(mAdapter);
 
         // Prepare the loader.  Either re-connect with an existing one,
         // or start a new one.
         getLoaderManager().initLoader(0, null, this);
+
+        //registerForContextMenu(getListView());
     }
 
     @Override
@@ -73,12 +80,21 @@ public class AccountsActivity extends ListActivity implements LoaderCallbacks<Cu
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.action_add:
+            showDialog(0, (String) null);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     // Called when a new Loader needs to be created
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         // Now create and return a CursorLoader that will take care of
         // creating a Cursor for the data being displayed.
-        return new CursorLoader(this, AccountProvider.CONTENT_URI,
-                PROJECTION, SELECTION, null, null);
+        return new CursorLoader(this, AccountProvider.CONTENT_URI, PROJECTION, SELECTION, null, null);
     }
 
     // Called when a previously created loader has finished loading
@@ -99,6 +115,77 @@ public class AccountsActivity extends ListActivity implements LoaderCallbacks<Cu
     @Override 
     public void onListItemClick(ListView l, View v, int position, long id) {
         // Do something when a list item is clicked
+        
+        Cursor cursor = (Cursor) getListView().getItemAtPosition(position);
+        long accountId = cursor.getLong(0);
+        String name = cursor.getString(1);
+        showDialog(accountId, name);
+    }
+
+    private void showDialog(final long id, final String name) {
+        String hint;
+        String text;
+        
+        final String hintAccountName = getString(R.string.hint_account_name);
+        final String actionOk = getString(R.string.action_ok);
+        final String actionCancel = getString(R.string.action_cancel);
+        
+        if (id > 0) {
+            text = name;
+            hint = name;
+        } else {
+            text = "";
+            hint = hintAccountName;
+        }
+        
+        final EditText editText = new EditText(this);
+        editText.setHint(hint);
+        editText.setText(text);
+
+        new AlertDialog.Builder(this)
+            //.setTitle("Edit Account Name")
+            .setMessage(hintAccountName)
+            .setView(editText)
+            .setPositiveButton(actionOk, new DialogInterface.OnClickListener() {
+    
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String newName = editText.getText().toString();
+                    String text;
+                    
+                    if (id > 0) {
+                        updateAccount(id, newName);
+                        text = "Changed " + name + " to " + newName;
+                    } else {
+                        insertAccount(newName);
+                        text = "Added " + newName;
+                    }
+                    
+                    Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+                }
+            })
+            .setNegativeButton(actionCancel, new DialogInterface.OnClickListener() {
+    
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(getApplicationContext(), "Cancelled", Toast.LENGTH_LONG).show();
+                }
+            })
+            .show();
+
+    }
+
+    private Uri insertAccount(String name) {
+        ContentValues values = new ContentValues();
+        values.put(ExpenseContract.Account.COLUMN_NAME_NAME, name);
+        return getContentResolver().insert(AccountProvider.CONTENT_URI, values);
+    }
+
+    private int updateAccount(long id, String name) {
+        ContentValues values = new ContentValues();
+        values.put(ExpenseContract.Account.COLUMN_NAME_NAME, name);
+        Uri uri = ContentUris.withAppendedId(AccountProvider.CONTENT_URI, id); 
+        return getContentResolver().update(uri, values, null, null);
     }
 
 }
--- src/com/example/expense/content/AccountProvider.java
+++ src/com/example/expense/content/AccountProvider.java
@@ -1,39 +1,114 @@
 package com.example.expense.content;
 
-import com.example.expense.data.AccountsDataSource;
-import com.example.expense.data.ExpenseDatabaseHelper;
-import com.example.expense.data.TransactionsDataSource;
-
 import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.provider.BaseColumns;
+import android.text.TextUtils;
+
+import com.example.expense.data.ExpenseContract;
+import com.example.expense.data.ExpenseDatabaseHelper;
 
 public class AccountProvider extends ContentProvider {
-    
-    public final static String AUTHORITY = "com.zacwhy.expense.provider";
-    public final static Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY  + "/accounts");
-    public final static Uri CONTENT_URI2 = Uri.parse("content://" + AUTHORITY  + "/transactions");
 
-    private ExpenseDatabaseHelper mOpenHelper;
-    
+    public static final String AUTHORITY = "com.zacwhy.expense.provider";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY  + "/" + Accounts.CONTENT_PATH);
+
+    // helper constants for use with the UriMatcher
+    private static final int ACCOUNT_LIST = 1;
+    private static final int ACCOUNT_ID = 2;
+
+    private static final UriMatcher URI_MATCHER;
+
+    public static interface Accounts extends BaseColumns {
+        public static final Uri CONTENT_URI = AccountProvider.CONTENT_URI;
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/vnd.expense.accounts";
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/vnd.expense.accounts";
+        public static final String CONTENT_PATH = "accounts";
+    }
+
+    static {
+        URI_MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
+        URI_MATCHER.addURI(AUTHORITY, Accounts.CONTENT_PATH, ACCOUNT_LIST);
+        URI_MATCHER.addURI(AUTHORITY, Accounts.CONTENT_PATH + "/#", ACCOUNT_ID);
+    }
+
+    private SQLiteDatabase mDb = null;
+
     @Override
     public boolean onCreate() {
-        mOpenHelper = new ExpenseDatabaseHelper(getContext());
+        ExpenseDatabaseHelper databaseHelper = new ExpenseDatabaseHelper(getContext());
+        mDb = databaseHelper.getWritableDatabase();
+
+        if (mDb == null) {
+            return false;
+        }
+
+        if (mDb.isReadOnly()) {
+            mDb.close();
+            mDb = null;
+            return false;
+        }
 
         return true;
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
+        if (URI_MATCHER.match(uri) != ACCOUNT_LIST) {
+            throw new IllegalArgumentException("Unsupported URI for insertion: " + uri);
+        }
+
+        long id = mDb.insert(ExpenseContract.Account.TABLE_NAME, null, values);
+
+        if (id > 0) {
+            // notify all listeners of changes and return itemUri:
+            Uri itemUri = ContentUris.withAppendedId(uri, id);
+            getContext().getContentResolver().notifyChange(itemUri, null);
+            return itemUri;
+        }
+
+        throw new SQLException("Problem while inserting");
     }
 
     @Override
-    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+    public int update(Uri uri, ContentValues values, String whereClause, String[] whereArgs) {
+        int updateCount = 0;
+
+        switch (URI_MATCHER.match(uri)) {
+
+        case ACCOUNT_LIST:
+            updateCount = mDb.update(ExpenseContract.Account.TABLE_NAME, values, whereClause, whereArgs);
+            break;
+
+        case ACCOUNT_ID:
+            String idString = uri.getLastPathSegment();
+            String where = Accounts._ID + " = " + idString;
+
+            if (!TextUtils.isEmpty(whereClause)) {
+                where += " AND " + whereClause;
+            }
+
+            updateCount = mDb.update(ExpenseContract.Account.TABLE_NAME, values, where, whereArgs);
+            break;
+
+        default:
+            throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
+
+        // notify all listeners of changes:
+        if (updateCount > 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return updateCount;
     }
 
     @Override
@@ -43,49 +118,41 @@ public class AccountProvider extends ContentProvider {
 
     @Override
     public String getType(Uri uri) {
-        return null;
+        switch (URI_MATCHER.match(uri)) {
+        case ACCOUNT_LIST:
+            return Accounts.CONTENT_TYPE;
+        case ACCOUNT_ID:
+            return Accounts.CONTENT_ITEM_TYPE;
+        default:
+            throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
     }
 
     @Override
-    public Cursor query(
-            Uri uri, 
-            String[] projection, 
-            String selection, 
-            String[] selectionArgs, 
-            String sortOrder) {
-        
-        int code = sUriMatcher.match(uri);
-        
-        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
-        
-        switch (code) {
-        
-        case 1:
-            AccountsDataSource accountsDataSource = new AccountsDataSource(db);
-            Cursor cursor = accountsDataSource.getCursor();
-            return cursor;
-            
-        case 2:
-            TransactionsDataSource transactionsDataSource = new TransactionsDataSource(db);
-            Cursor cursor2 = transactionsDataSource.getCursor(selection, selectionArgs);
-            return cursor2;
-            
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+        builder.setTables(ExpenseContract.Account.TABLE_NAME);
+
+//        if (TextUtils.isEmpty(sortOrder)) {
+//            sortOrder = null;
+//        }
+
+        switch (URI_MATCHER.match(uri)) {
+        case ACCOUNT_LIST:
+            // all nice and well
+            break;
+        case ACCOUNT_ID:
+            // limit query to one row at most:
+            builder.appendWhere(Accounts._ID + " = " + uri.getLastPathSegment());
+            break;
+        default:
+            throw new IllegalArgumentException("Unsupported URI: " + uri);
         }
-        
-        throw new IllegalArgumentException();
-        
-        //readableDatabase.close();
-        
-        //return cursor;
-    }
-    
-    // Creates a UriMatcher object.
-    private static final UriMatcher sUriMatcher;
-    
-    static {
-        sUriMatcher = new UriMatcher(0);
-        sUriMatcher.addURI(AUTHORITY, "accounts", 1);
-        sUriMatcher.addURI(AUTHORITY, "transactions", 2);
+
+        Cursor cursor = builder.query(mDb, projection, selection, selectionArgs, null, null, sortOrder);
+        // if we want to be notified of any changes:
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
     }
 
 }
